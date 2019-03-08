f22d8160fd76ecfef5d30ecc31c30f0dcce2d177
Alexander Mendelsohn <superparkourio@gmail.com>
2019-03-08 11:20:11 -0500

    Lab finished with comma removed.

---
+++ Lab03AlexanderMendelsohn.sql
@@ -0,0 +1,13 @@
+drop table if exists LabCreateTable;
+create table LabCreateTable (
+	id int identity,
+	code varchar(100)
+);
+insert into LabCreateTable values (CONCAT('CREATE TABLE ', (select top 1 TABLE_NAME from INFORMATION_SCHEMA.COLUMNS where table_name = 'Tavern'))), ('(');
+insert into LabCreateTable (code) select CONCAT('    ', COLUMN_NAME, ' ', DATA_TYPE, ',') from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'Tavern';
+update LabCreateTable set code = SUBSTRING(code, 1, LEN(code) - 1) where id = (select MAX(id) from LabCreateTable);
+alter table LabCreateTable drop column id;
+insert into LabCreateTable values (');');
+select * from LabCreateTable;
+
+select * from Tavern where name like '%Bucket';
